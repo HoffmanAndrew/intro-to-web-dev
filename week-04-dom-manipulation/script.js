cde672fdbe3d404a45cf9b39dd75b884319de04d
HoffmanAndrew <anhoffma@millersville.edu>
2022-02-17 17:59:30 -0500

    update

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -17,4 +17,56 @@ function fillOutputs() {
   const outputDiv = document.getElementById('output');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+
+
+
+
+       // Get the adjective input element
+      const animalOneInput = document.getElementById('animal-one-input');
+     // Get the text from the input element
+      const animalOne = animalOneInput.value;
+
+      // Get the adjective output element
+      const animalOneOutput = document.getElementById('animal-one-output');
+      // Set the text of the input element
+      animalOneOutput.innerText = animalOne;
+
+    
+
+
+    // Get the adjective input element
+  const verbOneInput = document.getElementById('verb-one-input');
+  // Get the text from the input element
+  const verbOne = verbOneInput.value;
+
+  // Get the adjective output element
+  const verbOneOutput = document.getElementById('verb-one-output');
+  // Set the text of the input element
+  verbOneOutput.innerText = verbOne;
+
+ 
+
+     // Get the adjective input element
+      const objectOneInput = document.getElementById('object-one-input');
+     // Get the text from the input element
+     const objectOne = objectOneInput.value;
+
+     // Get the adjective output element
+     const objectOneOutput = document.getElementById('object-one-output');
+     // Set the text of the input element
+     objectOneOutput.innerText = objectOne;
+
+
+
+
+   // Get the adjective input element
+  const prepositionOneInput = document.getElementById('preposition-one-input');
+  // Get the text from the input element
+  const prepositionOne = prepositionOneInput.value;
+
+  // Get the adjective output element
+  const prepositionOneOutput = document.getElementById('preposition-one-output');
+  // Set the text of the input element
+  prepositionOneOutput.innerText = prepositionOne;
+
 }
